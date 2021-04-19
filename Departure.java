9277fa035411e5cba4a04111d139ff0d2dfccf8f
palNisarga <61311493+palNisarga@users.noreply.github.com>
2021-04-19 09:05:42 +0530

    Add files via upload

---
+++ Departure.java
@@ -0,0 +1,32 @@
+package project1;
+import java.util.*;
+
+public class Depature {
+    public static void ToDepart(ArrayList<Integer> avail, List<Integer> occupied,Hashtable<Integer, List<String>> level_1) {
+        if(occupied.isEmpty()) {
+            System.out.println("No cars in the slot!");
+        }else {
+            System.out.println("Enter Slot no");    
+        Scanner input = new Scanner(System.in);
+        Integer slot_no = input.nextInt();
+        for( Map.Entry<Integer, List<String>> entry : level_1.entrySet() )
+            {
+                Integer key = entry.getKey();
+                List<String> values = entry.getValue();
+                if(key==slot_no)
+                {
+                System.out.print("Slot no:"+key+"\t");
+                System.out.print("Colour:"+values.get(2)+"\t");
+                System.out.println("Reg Plate:"+values.get(1));
+                level_1.remove(key);
+                occupied.remove(occupied.indexOf(slot_no));
+                    avail.add(slot_no);
+                }else {
+                    System.out.println("No cars in the slot!");
+                    break;
+                }
+            }
+        }
+    }
+     
+}
---
+++ Display_all.java
@@ -0,0 +1,22 @@
+package project1;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+public class Display{
+    public static void ShowAll(ArrayList<Integer> avail, List<Integer> occupied,Hashtable<Integer, List<String>> level_1) {
+        if(occupied.isEmpty()) {
+            System.out.println("No cars in the slot!");
+        }else {
+            for( Map.Entry<Integer, List<String>> entry : level_1.entrySet() )
+            {
+                Integer key = entry.getKey();
+                List<String> values = entry.getValue();
+                System.out.print("Slot no:"+key+"\t");
+                System.out.print("Colour:"+values.get(2)+"\t");
+                System.out.print("Reg Plate:"+values.get(1)+"\t");
+                System.out.print("Floor:"+values.get(3)+"\t");
+                System.out.println("Parking Date:"+values.get(4));
+            }
+        }
+    }
+}
