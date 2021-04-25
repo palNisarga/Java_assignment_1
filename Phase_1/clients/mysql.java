c1e6e5da284438816e28bc5246de58f1aa68fcf3
palNisarga <61311493+palNisarga@users.noreply.github.com>
2021-04-25 23:40:39 +0530

    Update mysql.java

--- Phase_1/clients/mysql.java
+++ Phase_1/clients/mysql.java
@@ -1,47 +1,21 @@
 package project1;
+
 import java.sql.*;
-
-public class sqldatabase {
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost:3306/test_db";
-   static final String USER = "root";
-   static final String PASS = "password";
-   
-   Connection conn = null;
-   Statement stmt = null;
-   {
-   try{
-      Class.forName("com.mysql.jdbc.Driver");
-
-      System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-      System.out.println("Creating database...");
-      stmt = conn.createStatement();
-      
-      String sql = "CREATE DATABASE STUDENTS";
-      stmt.executeUpdate(sql);
-      System.out.println("Database created successfully...");
-   }catch(SQLException se){
-      se.printStackTrace();
-   }catch(Exception e){
-      e.printStackTrace();
-   }finally{
-      try{
-         if(stmt!=null)
-            stmt.close();
-      }catch(SQLException se2){
-      }
-      try{
-         if(conn!=null)
-            conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }
-   }
-   System.out.println("Goodbye!");
-}
-}
+public class MySQL {
+	public Connection create() {
+		Connection con= null;
+		try{  
+			Class.forName("com.mysql.jdbc.Driver");  
+			con=DriverManager.getConnection(  
+					"jdbc:mysql://localhost:3306/test_db?characterEncoding=utf8","nisarga","qwertyuiop");    
+			
+			}
+		catch(Exception e){ 
+			System.out.println(e);
+			}
+		return con;
+	}
+	}
 
 
 
