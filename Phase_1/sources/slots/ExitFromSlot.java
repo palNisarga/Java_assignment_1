976bd4c9159cc3102741178c85881e4b1a7399de
palNisarga <61311493+palNisarga@users.noreply.github.com>
2021-04-25 23:51:22 +0530

    Update ExitFromSlot.java

--- Phase_1/sources/slots/ExitFromSlot.java
+++ Phase_1/sources/slots/ExitFromSlot.java
@@ -1,8 +1,8 @@
 package project1;
 import java.util.*;
 
-public class depature {
-    public static void to_depart(ArrayList<Integer> avail, List<Integer> occupied,Hashtable<Integer, List<String>> level_1) {
+public class ToRemoveFromSlot {
+    public static void RemoveFromSlot(ArrayList<Integer> avail, List<Integer> occupied,Hashtable<Integer, List<String>> level_1) {
         if(occupied.isEmpty()) {
             System.out.println("No cars in the slot!");
         }else {
