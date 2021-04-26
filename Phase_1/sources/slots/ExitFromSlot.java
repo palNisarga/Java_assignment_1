f3d4973fee018a8432427a0a1f6314c218e811e6
palNisarga <61311493+palNisarga@users.noreply.github.com>
2021-04-26 18:07:23 +0530

    Update ExitFromSlot.java

--- Phase_1/sources/slots/ExitFromSlot.java
+++ Phase_1/sources/slots/ExitFromSlot.java
@@ -2,31 +2,32 @@ package project1;
 import java.util.*;
 
 public class ToRemoveFromSlot {
-    public static void RemoveFromSlot(ArrayList<Integer> avail, List<Integer> occupied,Hashtable<Integer, List<String>> level_1) {
-        if(occupied.isEmpty()) {
-            System.out.println("No cars in the slot!");
-        }else {
-            System.out.println("Enter Slot no");    
+	public static void RemoveFromSlot(ArrayList<Integer> Avails, List<Integer> Occupied,Hashtable<Integer, List<String>> Level) {
+		if(Occupied.isEmpty()) {
+			System.out.println("No cars in the slot!");
+		}else {
+			System.out.println("Enter Slot no");	
         Scanner input = new Scanner(System.in);
-        Integer slot_no = input.nextInt();
-        for( Map.Entry<Integer, List<String>> entry : level_1.entrySet() )
-            {
-                Integer key = entry.getKey();
-                List<String> values = entry.getValue();
-                if(key==slot_no)
-                {
-                System.out.print("Slot no:"+key+"\t");
-                System.out.print("Colour:"+values.get(2)+"\t");
-                System.out.println("Reg Plate:"+values.get(1));
-                level_1.remove(key);
-                occupied.remove(occupied.indexOf(slot_no));
-                    avail.add(slot_no);
-                }else {
-                    System.out.println("No cars in the slot!");
-                    break;
-                }
-            }
-        }
-    }
-     
+		Integer slot_no = input.nextInt();
+	    for( Map.Entry<Integer, List<String>> entry : Level.entrySet() )
+	        {
+	            Integer key = entry.getKey();
+	            List<String> values = entry.getValue();
+	            if(key==slot_no)
+	            {
+	            System.out.print("Slot no:"+key+"\t");
+	            System.out.print("Colour:"+values.get(2)+"\t");
+	            System.out.println("Reg Plate:"+values.get(1));
+	            Level.remove(key);
+	            Occupied.remove(Occupied.indexOf(slot_no));
+	                Avails.add(slot_no);
+	            }else {
+	            	System.out.println("No cars in the slot!");
+	            	break;
+	            }
+	        }
+		}
+	}
+	 
 }
+
